99a19fe319ffa8117f43da0822774c0edc9712ca
Minah Shin <minah741@naver.com>
2024-07-25 20:26:12 +0900

    [level 0] Title: 두 수의 곱, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        // 1. num1과 num2를 곱하고, answer에 값을 할당한다.
+        answer = num1 * num2;
+        
+        return answer;
+    }
+}
