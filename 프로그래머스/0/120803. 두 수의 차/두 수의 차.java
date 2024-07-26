8923d9d2be20a855327958eb820a221e8b2413f5
Minah Shin <minah741@naver.com>
2024-07-26 15:13:19 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -1,8 +1,7 @@
-class Solution {
-    
+class Solution { 
     public int solution(int num1, int num2) {
         int answer = 0; // 답을 담아서 리턴하는 변수
-        // 1. num1에서 num2를 빼고, answer에 할당한다
+        // 1. 정수 num1에서 정수 num2를 뺀 식을 코드로 작성
         answer = num1 - num2;
         return answer; // 2. 1에서 계산된 값(answer)을 반환해준다
     }
